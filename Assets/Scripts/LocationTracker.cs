8999eb762444d8e5b77d0e25e359128bd9547265
NUVR <Discovery Lab@ARVR-2.nunet.neu.edu>
2019-10-03 20:40:43 -0400

    Set up script

--- Assets/Scripts/LocationTracker.cs
+++ Assets/Scripts/LocationTracker.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LocationTracker : MonoBehaviour
 {
+    [SerializeField]
+    public int MaxLocationServiceWait = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,43 +16,45 @@ public class LocationTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("What");
     }
 
     IEnumerator LocationGetter()
     {
         // First, check if user has location service enabled
         if (!Input.location.isEnabledByUser)
+        {
+            Debug.LogError("No location service found");
             yield break;
+        }
 
         // Start service before querying location
         Input.location.Start();
 
         // Wait until service initializes
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        while (Input.location.status == LocationServiceStatus.Initializing && MaxLocationServiceWait > 0)
         {
             yield return new WaitForSeconds(1);
-            maxWait--;
+            MaxLocationServiceWait--;
         }
 
         // Service didn't initialize in 20 seconds
-        if (maxWait < 1)
+        if (MaxLocationServiceWait < 1)
         {
-            print("Timed out");
+            Debug.LogError("Timed out");
             yield break;
         }
 
         // Connection has failed
         if (Input.location.status == LocationServiceStatus.Failed)
         {
-            print("Unable to determine device location");
+            Debug.LogError("Unable to determine device location");
             yield break;
         }
         else
         {
             // Access granted and location value could be retrieved
-            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
         }
 
         // Stop service if there is no need to query location updates continuously
