5e99f58690e7baaacfeded1ca06a6250648fffd9
Matthew DiLoreto <diloreto.m@northeastern.edu>
2019-11-07 21:19:42 -0500

    Add scripting for loading inventory

---
+++ Assets/Scripts/DataTypes/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+struct Inventory
+{
+    public Quest? ActiveQuest;
+    public int ActiveChunkIndex;
+
+    public Inventory(Quest? DefaultActiveQuest)
+    {
+        ActiveQuest = DefaultActiveQuest;
+        ActiveChunkIndex = 0;
+    }
+
+    public Chunk? GetActiveChunk()
+    {
+        if (!ActiveQuest.HasValue || ActiveChunkIndex >= ActiveQuest.Value.Chunks.Count)
+        {
+            return null;
+        }
+
+        return ActiveQuest.Value.Chunks[ActiveChunkIndex];
+    }
+
+    public LocationInfo? GetNextLocation()
+    {
+        Chunk? ActiveChunk = GetActiveChunk();
+        if (!ActiveChunk.HasValue)
+        {
+            return null;
+        }
+        return ActiveChunk.Value.Location;
+    }
+}
+
+
+
